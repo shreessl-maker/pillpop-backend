ec655d36f0d9e9c302df0b5714fd41dd7199c0c7
shreessl-maker <shreessl@gmail.com>
2025-12-08 10:34:34 +0530

    Create cloudinaryUpload.js

---
+++ utils/cloudinaryUpload.js
@@ -0,0 +1,21 @@
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadToCloudinary = async (filePath) => {
+  try {
+    const result = await cloudinary.uploader.upload(filePath, {
+      folder: "pharmiq",
+    });
+    return result.secure_url;
+  } catch (err) {
+    console.error("Cloudinary Upload Error:", err);
+    throw new Error("File upload failed");
+  }
+};
