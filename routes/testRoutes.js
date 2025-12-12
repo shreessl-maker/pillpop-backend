3cd2a837e121e2a69b9a303fc0520aea96b7ec0a
shreessl-maker <shreessl@gmail.com>
2025-12-13 00:32:39 +0530

    Update testRoutes.js

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -1,23 +1,31 @@
 import express from "express";
-import {
-  createTest,
-  updateTestStatus,
-  getAllTests,
-  getLiveTests,
-  getPastTests,
-  submitResult
-} from "../controllers/testController.js";
+import Test from "../models/Test.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-// admin
-router.post("/create", createTest);
-router.patch("/:id/status", updateTestStatus);
-router.get("/all/:clientId", getAllTests);
+// Get all tests
+router.get("/", protect, async (req, res) => {
+  const tests = await Test.find().populate("createdBy", "name").populate("assignedTo", "name");
+  res.json(tests);
+});
 
-// staff
-router.get("/live/:clientId", getLiveTests);
-router.get("/past/:staffId", getPastTests);
-router.post("/submit", submitResult);
+// Create test
+router.post("/", protect, async (req, res) => {
+  const test = await Test.create(req.body);
+  res.json(test);
+});
+
+// Update test
+router.put("/:id", protect, async (req, res) => {
+  const updated = await Test.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(updated);
+});
+
+// Delete test
+router.delete("/:id", protect, async (req, res) => {
+  await Test.findByIdAndDelete(req.params.id);
+  res.json({ message: "Test deleted" });
+});
 
 export default router;
